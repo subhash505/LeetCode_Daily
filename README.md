9b0d7f4ceaa3f8d1b462f6d29878844c626e3382
Mahesh Kakde <maheshkakde100@gmail.com>
2021-04-14 10:50:11 +0530

    palindromeNumber

--- README.md
+++ README.md
@@ -31,4 +31,12 @@ rows:
 
 string convert(string s, int numRows);
 
-*/
+
+Problem 09
+
+Palindrome Number
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. For example, 121 is palindrome while 123 is not.
+
--- palindromeNumber.java
+++ palindromeNumber.java
@@ -1,3 +1,13 @@
+// Problem 09
+
+/*
+Palindrome Number
+
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward. 
+For example, 121 is palindrome while 123 is not. */
+
 class Solution {
     public boolean isPalindrome(int x) {
 
