a5d2a11e79fb603effbef69a32183ffa66c6dc0e
Mahesh Kakde <maheshkakde100@gmail.com>
2021-05-01 23:25:11 +0530

    SubstringwithConcatenationofAllWords

--- substringwithConcatenationofAllWords.java
+++ substringwithConcatenationofAllWords.java
@@ -1,6 +1,15 @@
+/*
 #Problem 30
 
  Substring with Concatenation of All Words
+
+ ou are given a string s and an array of strings words of the same length. Return all starting indices of substring(s) in s that is a concatenation of each word in words exactly once, in any order, and without any intervening characters.
+
+You can return the answer in any order.
+
+
+
+ */
 
 
 class Solution {
