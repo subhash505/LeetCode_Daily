3ad5eaef203a38f57e9a5d58f2f5ec180573ca6f
Mahesh Kakde <maheshkakde100@gmail.com>
2021-07-19 23:56:09 +0530

    Update validSudoku.java

--- validSudoku.java
+++ validSudoku.java
@@ -1,9 +1,7 @@
 
 /*
 # Problem 36
-
-
-etermine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
 
 Each row must contain the digits 1-9 without repetition.
 Each column must contain the digits 1-9 without repetition.
