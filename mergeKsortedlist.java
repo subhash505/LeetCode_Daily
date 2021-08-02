826c74b88a74470a91efe934e2b4d167fe3713b8
Mahesh Kakde <maheshkakde100@gmail.com>
2021-08-02 23:49:29 +0530

    Update mergeKsortedlist.java

--- mergeKsortedlist.java
+++ mergeKsortedlist.java
@@ -1,6 +1,5 @@
 /* #Problem 23
-
-ou are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
+We are given an array of k linked-lists lists, each linked-list is sorted in ascending order.
 
 Merge all the linked-lists into one sorted linked-list and return it.
 
