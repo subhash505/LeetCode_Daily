7e4ef1d594162acab5ee8d61f065e507e613a984
Mahesh Kakde <maheshkakde100@gmail.com>
2021-08-19 23:43:23 +0530

    Update searchInsertPosition.java

--- searchInsertPosition.java
+++ searchInsertPosition.java
@@ -1,11 +1,8 @@
 Problem 35
-
-
 /*
 Given a sorted array of distinct integers and a target value,return the index 
 if the target is found.
 If not,return the index where it would be if it were inserted in order.
-
 */
 
 
