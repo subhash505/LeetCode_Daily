de5ad8639f639f5394036cd23003de979663630a
Mahesh Kakde <maheshkakde100@gmail.com>
2021-10-09 23:43:09 +0530

    Update sametree.java

--- sametree.java
+++ sametree.java
@@ -15,7 +15,6 @@ class Solution {
         } else if (p == null || q == null) {
             return false;
         }
-
         if (p.val == q.val) {
             return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
         } else {
