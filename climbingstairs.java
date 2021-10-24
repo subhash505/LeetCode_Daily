462d25b29d252d1db24040ebaafa30745ea8d541
Mahesh Kakde <maheshkakde100@gmail.com>
2021-10-24 23:50:11 +0530

    Update climbingstairs.java

--- climbingstairs.java
+++ climbingstairs.java
@@ -10,7 +10,7 @@ class Solution {
         if (n <= 1) {
             return 1;
         }
-        
+
         int[] ways = new int[n + 1];
         ways[0] = 1;
         ways[1] = 1;
@@ -19,6 +19,6 @@ class Solution {
             ways[i] = ways[i - 2] + ways[i - 1];
         }
         
-        return ways[n];
+       return ways[n];
     }
-}
+
