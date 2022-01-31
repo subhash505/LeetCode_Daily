316f736b622b3a284ce063dfdd40561f94313af6
Mahesh Kakde <maheshkakde100@gmail.com>
2022-01-31 23:05:56 +0530

    Update 4Sum.java

--- 4Sum.java
+++ 4Sum.java
@@ -9,7 +9,7 @@ public List<List<Integer>> fourSum(int[] nums, int target) {
             continue;
         for(int j=i+1; j<nums.length-2; j++){
             if(j!=i+1 && nums[j]==nums[j-1])
-                continue;
+              continue;
        int k=j+1;
             int l=nums.length-1;
             while(k<l){
@@ -35,6 +35,6 @@ public List<List<Integer>> fourSum(int[] nums, int target) {
                 }
             }
   }
-
     return result;
+
 }
