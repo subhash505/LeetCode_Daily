9d8a632f5b0a4a741ec726f0e030c093ffa60b06
Mahesh Kakde <maheshkakde100@gmail.com>
2022-02-01 23:17:00 +0530

    Update Can_Place_Flowers.java

--- January_2022_LeetCode_Challange/Can_Place_Flowers.java
+++ January_2022_LeetCode_Challange/Can_Place_Flowers.java
@@ -3,7 +3,7 @@ class Solution {
     int i = 0, count = 0;
     while (i < flowerbed.length) {
         if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)) {
-            flowerbed[i++] = 1;
+         flowerbed[i++] = 1;
             count++;
         }
          if(count>=n)
